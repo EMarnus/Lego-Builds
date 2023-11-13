fc0ef7de9dc72ca8287ac3189563af8472bbef76
Emile <bvcxzq1@gmail.com>
2023-11-13 22:06:48 +0000

    Add followers App

--- likes/views.py
+++ likes/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-from drf_api.permissions import IsOwnerOrReadOnly
+from drf_lego_api.permissions import IsOwnerOrReadOnly
 from .models import Like
 from .serializers import LikeSerializer
 
