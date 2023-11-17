129a73944b8501fc16601681aa1018db5dc0c7a2
Emile <bvcxzq1@gmail.com>
2023-11-17 18:05:37 +0000

    Change to Allowed Host code

--- drf_lego_api/settings.py
+++ drf_lego_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -37,7 +38,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-emarnus-pp5-8q1790mgsvc.ws-eu106.gitpod.io', 'lego-builds-284f09d1e728.herokuapp.com/']
+ALLOWED_HOSTS = ['8000-emarnus-pp5-8q1790mgsvc.ws-eu106.gitpod.io', os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
@@ -111,9 +112,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
